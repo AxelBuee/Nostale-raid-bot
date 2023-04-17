a8bc96c89fccf3fc674942e3dc1704d7bcff5673
Axel BUEE <axoudu77@gmail.com>
2023-04-17 22:31:13 +0200

    feat(bot): working slash commands with reactions

--- bot.py
+++ bot.py
@@ -1,57 +1,81 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-import asyncio
-import datetime
 from models.raid import Raid
-from typing import List
+from datetime import datetime, date, time
+from typing import Tuple
+import os
+from dotenv import load_dotenv
+import asyncio
 
-bot = commands.Bot(command_prefix='!')
-client = discord.Client()
+load_dotenv()
+#intents = discord.Intents(messages=True, reactions=True, guilds=True, members=True, presences=True, voice_states=True, typing=True, bans=True, emojis=True, integrations=True, webhooks=True, invites=True, voice_states=True, dm_typing=True, guild_typing=True, reactions=True, guild_reactions=True, messages=True, guild_messages=True, dm_messages=True, guild_typing=True, dm_typing=True, presences=True, guild_presences=True)
 
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', help_command=None, intents=intents)
 raids: dict[int, Raid] = {}
 
-# Check if it's past the start time plus 30 minutes
 
+@bot.event
+async def on_ready():
+    print("Bot is up and ready !")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
-@client.event
+@bot.event
 async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
+    print("ADD REACTION")
     raid = raids.get(reaction.message.id)
     if raid is None or user.bot:
         return
     message = reaction.message
-    if message.author != client.user:
+    if message.author != bot.user:
         return
     if str(reaction.emoji) == "✅":
         # Add user to participants list
         raid.add_participant(user)
-        await message.edit(content=f"Raid participants ({len(raid.participants)}/15):\n{raid.participants}")
+        await message.edit(content=str(raid))
 
-@client.event
+@bot.event
 async def on_reaction_remove(reaction: discord.Reaction, user: discord.User):
+    print("REMOVED REACTION")
     if user.bot:
         return
     raid = raids.get(reaction.message.id)
     if raid is not None:
-        await raid.remove_participant(user)
+        message = reaction.message
+        raid.remove_participant(user)
+        await message.edit(content=str(raid))
 
-@bot.command()
-async def clear(ctx, num_messages: int):
+@bot.tree.command(name="clear")
+@app_commands.describe(nb_of_messages_to_delete = "Number of messages to delete")
+async def clear(interaction: discord.Interaction, nb_of_messages_to_delete: int):
     """Clear a specified number of messages in the channel."""
-    if num_messages <= 0:
-        await ctx.send("Please enter a positive integer.")
+    if nb_of_messages_to_delete <= 0:
+        await interaction.response.send_message("Please enter a positive integer.", ephemeral=True)
         return
     try:
-        await ctx.channel.purge(limit=num_messages)
-        await ctx.send(f"Successfully cleared {num_messages} messages.")
+        await interaction.response.defer(ephemeral=True)
+        deleted = await interaction.channel.purge(limit=nb_of_messages_to_delete)
+        await interaction.followup.send(f"Successfully cleared {len(deleted)} messages.", ephemeral=True)
     except discord.Forbidden:
-        await ctx.send("I don't have permission to delete messages in this channel.")
+        await interaction.followup.send("I don't have permission to delete messages in this channel.", ephemeral=True)
     except discord.HTTPException:
-        await ctx.send("Failed to delete messages.")
+        await interaction.followup.send("Failed to delete messages.", ephemeral=True)
 
-@bot.command()
-async def start_raid(ctx):
-    # Your code to start a new raid event goes here
-    pass
+@bot.tree.command(name="start_raid")
+@app_commands.describe(raid_name = "Name of the raid", start_date = "Raid date", start_time = "Raid start time", max_participants = "Max number of participants")
+async def start_raid(interaction: discord.Interaction, raid_name: str, start_date: str, start_time: str, max_participants: int):
+    start_date_obj = datetime.fromisoformat(start_date).date()
+    start_time_obj = datetime.strptime(start_time, "%H:%M").time()
+    await interaction.response.send_message(f"Creating {raid_name} raid", ephemeral=True)
+    new_raid = Raid(raid_name, datetime.combine(start_date_obj,start_time_obj), max_participants)
+    message = await interaction.channel.send(str(new_raid))
+    new_raid.message=message
+    raids[message.id] = new_raid
 
-client.run('your_bot_token')
-bot.run("your_bot_token_here")
+bot.run(os.getenv("BOT_TOKEN"))
--- models/raid.py
+++ models/raid.py
@@ -1,13 +1,21 @@
 from discord import User
+import datetime
+from typing import List
 
 class Raid:
-    def __init__(self, date, time, message, max_participants):
-        self.date = date
-        self.time = time
+    def __init__(self, raid_name, start_datetime, max_participants, message=None):
+        self.raid_name: str = raid_name
+        self.start_datetime: datetime.datetime = start_datetime
         self.message = message
-        self.max_participants = max_participants
+        self.max_participants: int = max_participants
         self.participants = []
 
+    def __str__(self):
+        header = f"Session {self.raid_name}! \n Starting at : {self.start_datetime} \n {len(self.participants)}/{self.max_participants}"
+        participants = "\n".join(member.mention for member in self.participants)
+        header+=f":\n{participants}"
+        print(header)
+        return header
     def add_participant(self, user: User):
         if len(self.participants) < self.max_participants and user not in self.participants:
             self.participants.append(user)
@@ -15,8 +23,16 @@ class Raid:
         else:
             return False
         
-    def check_timeout():
-        if datetime.datetime.now() > start_time + datetime.timedelta(minutes=30):
+    def remove_participant(self, user: User):
+        if user in self.participants:
+            self.participants.remove(user)
+        
+    def check_timeout(self):
+        if datetime.datetime.now() > self.start_time + datetime.timedelta(minutes=30):
             return True
         else:
             return False
+    
+    def get_participant_list_pretty_print(self):
+        participants = "\n".join(member.mention for member in self.participants)
+        return f"Participants for {self.raid_name} ({len(self.participants)}/{self.max_participants}):\n{participants}"
